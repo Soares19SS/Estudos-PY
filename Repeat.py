d4acec27379984eefb7d230cf2a5dd41bb35ee93
Gabriel Soares <151212735+Soares19SS@users.noreply.github.com>
2025-10-01 18:51:19 -0300

    Add files via upload

---
+++ Repeat.py
@@ -0,0 +1,5 @@
+'''esse 5 signifca que ele vai repetir 5 vezes '''
+for i in range(5, 0, -1): 
+    print(i)
+
+''' tem que colocar o 6 ali porque o Python pede o último número, mas ele não inclui o ultimo numero, é uma regra aplicada também pra listas. '''
