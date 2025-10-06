dccee77016d62619b8c644f2cb7196d70a0d91be
Gabriel Soares <151212735+Soares19SS@users.noreply.github.com>
2025-10-06 15:18:46 -0300

    Add files via upload

---
+++ ldt.py
@@ -0,0 +1,10 @@
+frutas = ['Abacate', 'Banana', 'Morango']
+
+print(frutas[2]) # Baseado em index, inicia no zero.
+
+frutas.append('uva')
+frutas.append('Laranja')
+frutas.remove('Banana')
+print(len(frutas))
+
+print(frutas)
