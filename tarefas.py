07189037568476d63b78ec489fc84ba746e003d5
Gabriel Soares <151212735+Soares19SS@users.noreply.github.com>
2025-10-06 18:34:52 -0300

    Add files via upload

---
+++ tarefas.py
@@ -0,0 +1,10 @@
+tarefas = []
+
+tarefas.append('Estudar Python com IA')
+tarefas.append('Ler um artigo sobre IA')
+tarefas.append('Responder emails')
+tarefas.append('Lavar o carro')
+
+print('Minhas tarefas diárias:')
+for tarefa in tarefas:
+    print(f'Tarefa: {tarefa}')
